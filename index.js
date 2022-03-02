57dfa174ffdbf2472e837a0eec8ae26d65cc31dc
CSuperman <crlbusiness1@gmail.com>
2022-03-01 23:41:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// API to access Mcdonalds old toys.
+
+const res = require('express');
+
+const app = require('express')();
+const PORT = 3000;
+
+app.listen(
+    PORT,
+    () => console.log('it is located at http://localhost:${PORT}')
+)
+
+app.get('/retired', (req, res) => {
+    res.status(200).send({
+        name: {'peanuts': 'the adorable elephant'}
+    })
+});
