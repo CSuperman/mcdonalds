275f81e1486c7b374e72dede97e50ecbbe3aa9de
CSuperman <crlbusiness1@gmail.com>
2022-03-03 00:38:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.listen(
 
 app.get('/retired', (req, res) => {
     res.status(200).send(retired = {
-            Mario = {'descr': 'One of the most popular video game characters of all time. There are 8 different Mario figurines.', 'date': null, 'pic': null, 'price': '250.00 a set'},
-            TeenieBeanieBoos = {'descr': 'Similar to Beanie Baby cousins. There are 4 of these in a set.', 'date': null, 'pic': null, 'price': '450.00 each'},
-            Furby = {'descr': 'Children across the United States were fascinated with these tiny stuffed toys. There are 4 Furbies. ', 'date': null, 'pic': null, 'price': '120.00 each'}
+            Mario: {'descr': 'One of the most popular video game characters of all time. There are 8 different Mario figurines.', 'date': null, 'pic': null, 'price': '250.00 a set'},
+            TeenieBeanieBoos: {'descr': 'Similar to Beanie Baby cousins. There are 4 of these in a set.', 'date': null, 'pic': null, 'price': '450.00 each'},
+            Furby: {'descr': 'Children across the United States were fascinated with these tiny stuffed toys. There are 4 Furbies. ', 'date': null, 'pic': null, 'price': '120.00 each'}
     })
 });
