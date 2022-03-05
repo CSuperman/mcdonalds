de49b78e5d8e07fc2b597b6bf65e57f02146840c
CSuperman <crlbusiness1@gmail.com>
2022-03-04 22:24:56 -0500

    Create server.js
    Switched server tech for a rapid testing goal.

---
+++ server.js
@@ -0,0 +1,17 @@
+// API to access old Mcdonalds toys
+
+const app = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const PORT = process.env.port || 3000;
+
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(
+    PORT,
+    () => console.log('it is located at http://localhost:${PORT}')
+);
