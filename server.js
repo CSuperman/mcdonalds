3c68e0f51bab7719a7f030bcde972a50b08eb5c6
CSuperman <crlbusiness1@gmail.com>
2022-04-23 23:44:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,4 @@ const PORT = process.env.port || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(
-    PORT,
-    () => console.log('it is located at http://localhost:${PORT}')
-);
 
-app.all('*', (req, res) => {
-  res.status(404).sendFile('/error.html');
-});
